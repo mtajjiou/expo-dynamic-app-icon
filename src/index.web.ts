4cb189374ac28b1f8d5ec756a470e5ab27f1de73
Samuel Newman <mozzius@protonmail.com>
2024-11-27 16:28:16 +0000

    web compatability

---
+++ src/index.web.ts
@@ -0,0 +1,7 @@
+export function setAppIcon(name: string | null): string | false {
+  throw new Error("setAppIcon is not supported on web");
+}
+
+export function getAppIcon(): string {
+  throw new Error("getAppIcon is not supported on web");
+}
