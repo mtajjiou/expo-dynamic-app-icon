fd4ea7c8ad25281d6c51757987c5e24f2c23cc2f
Josh Pensky <joshuapensky@gmail.com>
2024-11-29 12:42:26 -0500

    update .web.ts for compat

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,7 +1,13 @@
-export function setAppIcon(name: string | null): string | false {
+import { DynamicAppIconRegistry } from "./types";
+
+export type IconName = DynamicAppIconRegistry["IconName"];
+
+export function setAppIcon(
+  name: IconName | null
+): IconName | "DEFAULT" | false {
   throw new Error("setAppIcon is not supported on web");
 }
 
-export function getAppIcon(): string {
+export function getAppIcon(): IconName | "DEFAULT" {
   throw new Error("getAppIcon is not supported on web");
 }
